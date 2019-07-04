f2b378f8cbf510d6f6f785dcd934f832ede6dfe6
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-07-03 23:54:01 -0300

    adicionado validação

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -2,4 +2,8 @@ class Advert < ApplicationRecord
   belongs_to :user
   belongs_to :category
   mount_uploaders :images, PhotosUploader
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :price
+  validates_presence_of :images
 end
