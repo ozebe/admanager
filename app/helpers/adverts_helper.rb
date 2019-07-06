58e39e91ca6230f1e30cb028a48a59d1e126c275
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-07-06 15:20:26 -0300

    adicionado helpers para resumir o titulo e também alterar a forma que a data é visualizada

--- app/helpers/adverts_helper.rb
+++ app/helpers/adverts_helper.rb
@@ -1,2 +1,11 @@
 module AdvertsHelper
+  def post_date(date)
+    # formatting date: Aug, 31 2007 - 9:55PM
+    date.strftime("%b, %d %H:%M")
+  end
+
+  def resume(text)
+    # text[0..300] + "..."
+    truncate(text, length: 30)
+  end
 end
