c749f8cbbd7945fd0ae5551c72cf112a6ff5ece6
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-07-07 20:51:41 -0300

    melhorado os controladores e buscas

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -8,36 +8,57 @@ class AdvertsController < ApplicationController
 
     if params[:titleSearch].nil?
       if params[:category_id].nil?
-        @adverts = Advert.all
-        @adverts = Advert.order(:views).order(:title)
-        @categories = Category.all
+        if params[:id].nil?
+          @adverts = Advert.all
+          @adverts = Advert.order(:views).order(:title)
+          @categories = Category.all
+        else
+          @adverts = Advert.where(:user_id => params[:id]).order(:views).order(:title)
+          @categories = Category.all
+        end
+
       else
-        @adverts = Advert.where(:category_id => params[:category_id]).order(:views).order(:title)
-        @categories = Category.all
-        flash[:notice] = "Existem <b>#{@adverts.count}</b> anúncios nessa categoria".html_safe
+        if params[:id].nil?
+          @adverts = Advert.where(:category_id => params[:category_id]).order(:views).order(:title)
+          @categories = Category.all
+          flash[:notice] = "Existem <b>#{@adverts.count}</b> anúncios nessa categoria".html_safe
+        else
+          @adverts = Advert.where(:category_id => params[:category_id]).order(:views).order(:title)
+          @adverts = Advert.where(:user_id => params[:id]).order(:views).order(:title)
+          @categories = Category.all
+        end
+
       end
 
     else
       if params[:category_id].nil?
-        @adverts = Advert.titleSearch(params[:titleSearch]).order(:views).order(:title)
-        @categories = Category.all
+        if params[:id].nil?
+          @adverts = Advert.titleSearch(params[:titleSearch]).order(:views).order(:title)
+          @categories = Category.all
+        else
+          @adverts = Advert.titleSearch(params[:titleSearch]).order(:views).order(:title)
+          @adverts = Advert.where(:user_id => params[:id]).order(:views).order(:title)
+          @categories = Category.all
+        end
+
       else
-        @adverts = Advert.where(:category_id => params[:category_id]).order(:views).order(:title)
-        @adverts = Advert.titleSearch(params[:titleSearch]).order(:views).order(:title)
-        @categories = Category.all
-        flash[:notice] = "Existem <b>#{@adverts.count}</b> anúncios nessa categoria".html_safe
+        if params[:id].nil?
+          @adverts = Advert.where(:category_id => params[:category_id]).order(:views).order(:title)
+          @adverts = Advert.titleSearch(params[:titleSearch]).order(:views).order(:title)
+          @categories = Category.all
+          flash[:notice] = "Existem <b>#{@adverts.count}</b> anúncios nessa categoria".html_safe
+        else
+          @adverts = Advert.where(:category_id => params[:category_id]).order(:views).order(:title)
+          @adverts = Advert.titleSearch(params[:titleSearch]).order(:views).order(:title)
+          @adverts = Advert.where(:user_id => params[:id]).order(:views).order(:title)
+          @categories = Category.all
+          flash[:notice] = "Existem <b>#{@adverts.count}</b> anúncios nessa categoria".html_safe
+        end
+
       end
 
     end
 
-    if params[:id].nil?
-      @adverts = Advert.all
-      @adverts = Advert.order(:views).order(:views).order(:title)
-      @categories = Category.all
-    else
-      @adverts = Advert.where(:user_id => params[:id]).order(:views).order(:title)
-      @categories = Category.all
-    end
 
     end
 
