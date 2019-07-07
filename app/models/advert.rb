e4109f94b5a02ff78d1dbc6d23659706a29d7d15
Wesley Ozebe <wesleysantos345@hotmail.com>
2019-07-07 20:16:58 -0300

    criado a função de incrementar visualizações no no anúncio

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -8,4 +8,10 @@ class Advert < ApplicationRecord
   validates_presence_of :images
 
   scope :titleSearch, -> (title) { where("UPPER (adverts.title) like ?", "%#{title.upcase}%")}
+
+  def increment(by = 1)
+    self.views ||= 0
+    self.views += by
+    self.save
+  end
 end
